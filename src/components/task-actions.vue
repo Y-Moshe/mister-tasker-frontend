153f3b79cd24b6cbb0adcf6ca890c7a6a53169fe
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-15 21:43:10 +0200

    task-actions

---
+++ src/components/task-actions.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { defineProps } from 'vue'
+defineProps({
+  task: Object
+})
+</script>
+
+<template>
+  <section class="table-actions">
+    <el-button>Start task</el-button>
+    <router-link to="/">
+      <router-link :to="'/task/edit/' + task._id">
+        <el-button plain>Update</el-button>
+      </router-link>
+      <el-popconfirm
+        :icon="null"
+        confirm-button-text="Yes" 
+        title="Are you sure to delete this?"
+        @confirm="$emit('delete')">
+        <template #reference>
+          <el-button type="danger" plain>Delete</el-button>
+        </template>
+      </el-popconfirm>
+    </router-link>
+  </section>
+</template>
