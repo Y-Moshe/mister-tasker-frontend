563082d346390b30774f475bbfa9c6502287dff2
Y-Moshe <moshe.nehemiah@gmail.com>
2023-02-03 16:40:24 +0200

    using momentjs

--- frontend/src/components/task-preview.vue
+++ frontend/src/components/task-preview.vue
@@ -1,12 +1,16 @@
 <script setup>
+import { computed } from 'vue'
+import moment from 'moment'
 import { Delete } from '@element-plus/icons-vue'
 import { taskService } from '../services/task.service'
-
-defineProps({
-  task: Object
-})
+const props = defineProps({ task: Object })
 
 const STATUS = taskService.STATUS
+const getTime = (timpestamp) => timpestamp && moment(new Date(timpestamp)).fromNow()
+
+const createdAt = computed(() => getTime(props.task.createdAt))
+const lastTriedAt = computed(() => getTime(props.task.lastTriedAt))
+const doneAt = computed(() => getTime(props.task.doneAt))
 </script>
 
 <template>
@@ -15,11 +19,21 @@ const STATUS = taskService.STATUS
     <p>{{ task.description }}</p>
     <h3>Errors:</h3>
     <p>{{ task.errors.join(', ') }}</p>
-    <p>Created at: {{ task.createdAt }} | Last tried at: {{ task.lastTriedAt }} | Done at: {{ task.doneAt }}</p>
+    <p>
+      <span v-if="createdAt">Created at: {{ createdAt }}</span>
+      <template v-if="lastTriedAt">
+        <el-divider direction="vertical" />
+        <span>Last tried at: {{ lastTriedAt }}</span>
+      </template>
+      <template v-if="doneAt">
+        <el-divider direction="vertical" />
+        <span>Done at: {{ doneAt }}</span>
+      </template>
+    </p>
     <el-popconfirm
       v-if="task.status === STATUS.DONE"
       :icon="null"
-      confirm-button-text="Yes" 
+      confirm-button-text="Yes"
       title="Are you sure you want to delete this?"
       @confirm="$emit('delete', task._id)">
       <template #reference>
